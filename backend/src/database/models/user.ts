d8859d346da2e5d5076da70ca21174688d21abaf
its-rayan <rayanmabdirahman@gmail.com>
2025-03-18 22:59:01 +0000

    Add mongoose user schema

---
+++ backend/src/database/models/user.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true
+  },
+  avatar: {
+    type: String
+  },
+  collections: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Collection'
+    }
+  ]
+});
+
+export default mongoose.models.User || mongoose.model('User', userSchema);
