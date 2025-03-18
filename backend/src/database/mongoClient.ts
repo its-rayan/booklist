2dac913f8eb582498a7be8f263cce367ab086eb8
its-rayan <rayanmabdirahman@gmail.com>
2025-03-18 22:29:12 +0000

    Use mongoose to connect to db

---
+++ backend/src/database/mongoClient.ts
@@ -0,0 +1,32 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import config from '../config';
+import logger from '../logger';
+
+if (!config.db.uri) {
+  throw new Error(`Invalid/Mssing environment variable: MONGO_URI`);
+}
+
+const client = new MongoClient(config.db.uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+// This is need to be used as an adapter for Authjs to work.
+// TODO - Remove this if not needed
+const connectMongoClient = async (): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    try {
+      client.connect();
+      logger.info(`✅ Successfully connected to database`);
+      resolve(`Successfully connected to database`);
+    } catch (error) {
+      logger.error(`🚨 Failed to connect to database: ${error}`);
+      reject(`Failed to connect to database: ${error}`);
+    }
+  });
+};
+
+export default connectMongoClient;
