09a456b14347835d871c7bf88e7d3c9aafe1b3c0
its-rayan <rayanmabdirahman@gmail.com>
2025-03-23 02:09:57 +0000

    Add functionality to check and add user to DB

--- backend/src/api/v1/auth/controller.ts
+++ backend/src/api/v1/auth/controller.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { signInSchema } from './schemas';
 import { StatusCodes } from 'http-status-codes';
 import logger from '../../../logger';
+import User from '../../../database/models/user';
 
 export const signIn = async (req: express.Request, res: express.Response) => {
   try {
@@ -16,6 +17,23 @@ export const signIn = async (req: express.Request, res: express.Response) => {
         error: message
       });
     }
+
+    // Check if user exists
+    const userInDb = await User.findOne({ email: model.email });
+    if (userInDb) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        status: 'error',
+        error: 'User already exists'
+      });
+    }
+
+    const user = new User(model);
+    await user.save();
+
+    res.status(StatusCodes.CREATED).json({
+      status: 'success',
+      data: user
+    });
   } catch (error) {
     logger.error(error);
     res.status(StatusCodes.BAD_REQUEST).json({
