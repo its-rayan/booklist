62cd5f25094e8b19f9b6fecdc4a29a21168786a6
its-rayan <rayanmabdirahman@gmail.com>
2025-03-25 23:57:54 +0000

    Add sign in request validation schema

--- backend/src/api/v1/auth/schemas.ts
+++ backend/src/api/v1/auth/schemas.ts
@@ -4,3 +4,7 @@ export const signUpSchema = z.object({
   name: z.string().min(1).max(255),
   email: z.string().email()
 });
+
+export const signInSchema = z.object({
+  email: z.string().email()
+});
