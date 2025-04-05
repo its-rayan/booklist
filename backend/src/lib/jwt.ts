d5810f346d2e21b972c4e2095f2eb8a5766cf121
its-rayan <rayanmabdirahman@gmail.com>
2025-04-05 01:28:58 +0100

    Extend express request to include user data

--- backend/src/lib/jwt.ts
+++ backend/src/lib/jwt.ts
@@ -1,11 +1,20 @@
 import jwt from 'jsonwebtoken';
+import { Types } from 'mongoose';
 
-export const signJwt = (payload: string | object) => {
+export interface signJwtPayload {
+  id: Types.ObjectId;
+}
+
+export interface JwtPayload extends jwt.JwtPayload {
+  id: string;
+}
+
+export const signJwt = (payload: signJwtPayload) => {
   return jwt.sign(payload, `${process.env.JWT_SECRET}`, {
     expiresIn: parseInt(`${process.env.JWT_EXPIRES_IN}`)
   });
 };
 
-export const verifyJwt = (token: string) => {
-  return jwt.verify(token, `${process.env.JWT_SECRET}`);
+export const verifyJwt = (token: string): JwtPayload => {
+  return jwt.verify(token, `${process.env.JWT_SECRET}`) as JwtPayload;
 };
