a50ecb4a78caecae35c0dc3a4558c312bd0bf625
its-rayan <rayanmabdirahman@gmail.com>
2025-04-17 20:03:48 +0100

    Setup tailwindcss and shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <p>Hello world</p>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
