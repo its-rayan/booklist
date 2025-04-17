bb4e9187ca16a1da4cfe5b802f6d0cee58440ebe
its-rayan <rayanmabdirahman@gmail.com>
2025-04-17 22:25:12 +0100

    Added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,28 @@
-import { Button } from "@/components/ui/button";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
+import Login from "./features/auth/pages/login";
+import Register from "./features/auth/pages/register";
+
+const Home = () => <h2>Home Page</h2>;
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button>Click me</Button>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
