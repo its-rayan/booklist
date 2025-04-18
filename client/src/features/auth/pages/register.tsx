4d221f316b8f6868c47a98c7862f762c26149e33
its-rayan <rayanmabdirahman@gmail.com>
2025-04-18 13:59:49 +0100

    Added register page layout

--- client/src/features/auth/pages/register.tsx
+++ client/src/features/auth/pages/register.tsx
@@ -1,5 +1,99 @@
-import React from "react";
+import { Link } from "react-router";
+import AuthLayout from "../layout";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-const Register = () => <h1>Register Page</h1>;
+const Register = () => {
+  const form = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data: unknown) => {
+    console.log(data);
+  };
+
+  return (
+    <AuthLayout>
+      {/* Heading */}
+      <div className="border-b border-b-neutral-200 pb-4 mb-4">
+        <h1 className="text-xl font-medium">Register</h1>
+        <p className="text-neutral-500">
+          Already have an account?{" "}
+          <Link to="/login">
+            <span className="underline">Login</span>
+          </Link>{" "}
+          .
+        </p>
+      </div>
+      {/* Heading */}
+
+      {/* Form */}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-neutral-500">Username</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="john_doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-neutral-500">Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="john@doe.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-neutral-500">Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="********" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className="w-full cursor-pointer" type="submit">
+            Register
+          </Button>
+        </form>
+      </Form>
+      {/* Form */}
+    </AuthLayout>
+  );
+};
 
 export default Register;
